0dc51895aec5a5f2c7a232c4181200a1cc83619a
mirubid <github.10.dmil@xoxy.net>
2015-01-17 20:30:56 -0800

    pave debug

--- src/pave.lua
+++ src/pave.lua
@@ -1,36 +1,36 @@
 -- pave
-
+if(nil==deque) then os.loadAPI("deque") end
 local targs = {...}
 local dimensions = deque.new()
 local i,v,shape,x,y
 local start_pos=nav.getPos()
 local start_bearing=nav.getBearing()
 
-local shapes={
-	rectangle=rect_iter
-}
+
+print ("starting pave")
 if(#targs == 0) then
 	shape="rectangle"
-	dimensions.push_right(8)
-	dimensions.push_right(8)
-	return
+	dimensions:push_right(8)
+	dimensions:push_right(8)
 end
 for i,v in pairs(targs) do
 	local n = tonumber(v)
 	if(n) then 
-		dimensions.push_right(n) 	
+		dimensions:push_right(n) 	
 	else
 		shape = v
 	end
 end
-
+if(not shape) then
+	shape="rectangle"
+end
 
 print ( "paving a "..shape)
 function pave_down()
 	turtle.placeDown()
 end
 function rect_iter(dimensions,f)
-	local z,x= dimensions[0], dimensions[1]
+	local z,x= dimensions[1], dimensions[2]
 	local x_bearing = 1
 	if ( not z or not x) then
 		print("two dimensions required")
@@ -38,41 +38,58 @@ function rect_iter(dimensions,f)
 	end
 	
 	z = math.abs(z)
+	print(z.."x"..x.." rectangle")
+	
 	if(x<0) then
 		x_bearing=-1
 	end
-	local i,j,di,dj = 0,0,1,1
-	local function z()
-		i = i + dj
+	local i,j,di,dj = 01,1,1
+	local function iter_z()
+		i = i + di
+		
 		if(i>z or i<1) then
-			dj = -dj
-			return false
+			di = -di
+			return nil
 		end
+		print("z: "..i..","..j.."("..di..")")
 		local bearing = start_bearing
-		if(dj < 0) then
+		if(di < 0) then
 			bearing = (start_bearing + 2) % 4
 		end
 		if( nav.moveDir(bearing) ) then
 			f()
 			return true
 		else
-			return false, "failed to move"
+			return nil, "failed to move"
 		end
 	end
-	local function z()
+	local function iter_x()
 		j = j + dj
 		if (j>math.abs(x) ) then
-			return false
+			return nil
 		end
-		if(nav.moveDir(x_bearing))
-		f()
-		return true
+		print("x: "..i..","..j.."("..dj..")"..x_bearing)
+		if(nav.moveDir(x_bearing)) then
+		
+			f()
+			return true
+		end
+		return nil
 	end
 	return function()
-		return (z() or x())
+		if ( (iter_z() or iter_x()) ) then
+			return true
+		else
+			return nil
+		end
 	end
 end
 
 function wall_iter(dimensions,f)
 	return nil
 end
+local shapes={
+	rectangle=rect_iter
+}
+local _
+for _ in shapes[shape](dimensions,function() os.sleep(2) end) do end
