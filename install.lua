eac753f70c159205602512e8434dc6282b12a2e1
mirubid <github.10.dmil@xoxy.net>
2015-07-13 21:31:11 -0700

    implement install

--- install.lua
+++ install.lua
@@ -1,5 +1,23 @@
 local host="http://mirubid.github.io/mc-turtles/"
 print("loading from " .. host .. "...")
 local manifest = http.get(host .. "manifest.txt")
+
+if (not fs.exists("/api")) then
+	fs.makeDir("/api")
+end
+local function dl(fn)
+	local src = http.get(host .."src/" .. fn)
+	local lfn = string.gsub(fn,"\.lua$","")
+	local f = fs.open(lfn,"w")
+	f.write(src.readAll())
+	f.close()
+end
+
+local fn = manifest.readLine()
+while(fn) do
+	print("file " .. fn)
+	dl(fn)
+	fn = manifest.readLine()
+end
 print(manifest.readAll())
 print("done")
